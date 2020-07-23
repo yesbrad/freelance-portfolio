96be51c6b7475889975e17700a8187a3f682d432
brad <b.bradfrancis@gmail.com>
2020-07-23 20:56:38 +1000

    Added Basic Header

---
+++ src/components/home/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './styles.scss';
+import Button from '../button';
+
+const Home = () => {
+	return (
+		<div className="home-container">
+			<div className="home-content-container">
+				<div class="home-content-gradient">
+					<div className="home-content-header">
+						<h2>BRAD FRANCIS</h2>
+						<h1>WEB DEVELOPER</h1>
+					</div>
+					<nav>
+						<div className="home-content-nav-container">
+							<button>SERVICES</button>
+							<button>ABOUT</button>
+							<button>PROJECTS</button>
+							<div className="home-content-nav-action"><Button title="Get a Quote"/></div>
+						</div>
+					</nav>
+					<div className="home-content-action-buttons">
+						<div className="home-content-button">
+							<Button title="Get a Quote" />
+						</div>
+						<div className="home-content-button">
+							<Button title="Services" />
+						</div>
+					</div>
+					<div className="home-content-hook">
+						<span>NEED A ONLINE PROFESSINAL PRESENCE?</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Home;
