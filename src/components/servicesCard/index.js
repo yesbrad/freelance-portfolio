7504fc63de38954751324474b133c5cbba5f15b1
brad <b.bradfrancis@gmail.com>
2020-07-25 20:35:59 +1000

    Added Reveal Animation

--- src/components/servicesCard/index.js
+++ src/components/servicesCard/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './styles.scss';
+import { animated, useSpring, config } from 'react-spring';
 
 const ServicesCard = ({ serviceInfo }) => {
 	const { title, suits, image, description } = serviceInfo;
+	const ref = useRef();
+	const [pos, setPos] = useState(false);
+
+	const handleScroll = () => {
+		const positionEl = ref.current.getBoundingClientRect().y;
+		const posOn = (window.innerHeight - window.innerHeight / 4 > (positionEl));
+		// console.log(window.innerHeight, positionEl, posOn);
+		setPos(posOn);
+	};
+	
+	useEffect(() => {
+		console.log(window.innerHeight);
+
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	const renderSuits = suits.map((data) => (
 		<div className="services-card-suit">
@@ -10,19 +29,24 @@ const ServicesCard = ({ serviceInfo }) => {
 		</div>
 	))
 	
+	const spring = useSpring({
+		transform: pos ? 'translateY(0)' : 'translateY(40px)',
+		// opacity: pos ? 1 : 0,
+		// delay: 50,
+		config: {...config.wobbly, tension: 300, friction: 7}
+	});
+
 	return (
-		<div className="services-card-container">
-			<div className="services-card-content">
+		<div ref={ref} className="services-card-container">
+			<animated.div style={spring} className="services-card-content">
 				<h2>{title}</h2>
 				<div className="services-card-suit-container">
 					{renderSuits}
 				</div>
 				<div className="services-card-description">
-					<span>
-						{description}
-					</span>
+					<span>{description}</span>
 				</div>
-			</div>
+			</animated.div>
 		</div>
 	)
 }
