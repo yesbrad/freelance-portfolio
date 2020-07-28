02e8688c7ae792171313b5850be98212a482119b
brad <b.bradfrancis@gmail.com>
2020-07-28 16:42:41 +1000

    Services Content Pass

--- src/components/servicesCard/index.js
+++ src/components/servicesCard/index.js
@@ -4,7 +4,7 @@ import { animated, useSpring, config } from 'react-spring';
 import { useReveal } from '../../hooks';
 
 const ServicesCard = ({ serviceInfo, style }) => {
-	const { title, suits, image, description } = serviceInfo;
+	const { title, suits, image, description, included } = serviceInfo;
 	const ref = useRef();
 	const reveal = useReveal(ref);
 
@@ -24,11 +24,19 @@ const ServicesCard = ({ serviceInfo, style }) => {
 		<div ref={ref} key={title} className="services-card-container">
 			<animated.div key={title} style={style} className="services-card-content">
 				<h2>{title}</h2>
+				<div style={{background: `url(${image})`, backgroundPosition: 'center', backgroundSize: 'cover'}} className="services-image"></div>
+				<h3>Suitible For:</h3>
 				<div className="services-card-suit-container">
 					{renderSuits}
 				</div>
 				<div className="services-card-description">
-					<span>{description}</span>
+					{description.split('\n').map((item, i) => {
+						return <span key={i}>{item}</span>;
+					})}
+				</div>
+				<h3>Services Included:</h3>
+				<div className="services-included">
+					{included.map(inc => <span>- {inc}</span>)}
 				</div>
 			</animated.div>
 		</div>
--- src/data/services/index.js
+++ src/data/services/index.js
@@ -1,39 +1,73 @@
 const data = [
 	{
 		title: 'Website',
-		image: '',
+		image: require('../../images/services/Web.png'),
 		suits: [
-			'Professinals',
+			'Professionals',
 			'Bands',
 			'Businesses',
 			'Creatives',
 			'Retailers'
 		],
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit purus dignissim porta dictum. Proin nec urna ut sem dictum condimentum eget sed tortor. Vivamus non enim at est luctus auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec porta sapien et arcu tincidunt varius. Suspendisse et scelerisque tellus. Nullam sed posuere nunc. Ut vel risus ligula. Sed a lacus id neque varius mattis. Cras vel lacus neque. Curabitur dictum neque scelerisque ex ultricies molestie. Integer quis ipsum sed massa rutrum euismod. Phasellus posuere massa ac risus posuere vestibulum. Nam quis ante mauris. Duis tellus est, aliquam in mauris nec, vehicula consectetur dui. Etiam bibendum eget quam quis porta. '
+		included: [
+			'Design',
+			'Development',
+			'Hosting',
+			'Free Domain',
+			'SEO',
+			'Updates',
+		],
+		description: `A Custom Website specifically tailored to you or your business needs. All websites cab be designed, developed, hosted by me. I'll also be happy to work on any future changes you require on the site.
+\nAll sites are designed with SEO in mind giving your site the best chance of ranking in Google, Bing, etc.
+\nAll sites a responsive and mobile friendly. Making them work on any screen size.
+\nWebsites are a critical part of any business. Its often the first place potential new customers and clients will discover you. So a modern, professional and trust worthy site will help land those customers and clients.
+		`,
 	},
 	{
 		title: 'Online Store',
-		image: '',
+		image: require('../../images/services/Store.png'),
 		suits: [
-			'Professinals',
-			'Bands',
+			'Entrepreneurs',
+			'Merch',
 			'Businesses',
 			'Creatives',
-			'Retailers'
+			'Retailers',
+			'Brands'
 		],
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit purus dignissim porta dictum. Proin nec urna ut sem dictum condimentum eget sed tortor. Vivamus non enim at est luctus auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec porta sapien et arcu tincidunt varius. Suspendisse et scelerisque tellus. Nullam sed posuere nunc. Ut vel risus ligula. Sed a lacus id neque varius mattis. Cras vel lacus neque. Curabitur dictum neque scelerisque ex ultricies molestie. Integer quis ipsum sed massa rutrum euismod. Phasellus posuere massa ac risus posuere vestibulum. Nam quis ante mauris. Duis tellus est, aliquam in mauris nec, vehicula consectetur dui. Etiam bibendum eget quam quis porta. '
+		included: [
+			'Design',
+			'Development',
+			'Hosting',
+			'Free Domain',
+			'SEO',
+			'Design Updates',
+			'Shopify Account Support',
+		],
+		description: `A Custom E-Commerce Store specific for what ever your trying to sell. The store will be integrated with Shopify, one of the world leading store providers.
+		\nAll stores are designed with SEO in mind giving your site the best chance of ranking in Google, Bing, etc.
+		\nAll stores a responsive and mobile friendly. Making them work on any screen size.
+		`
 	},
 	{
 		title: 'App',
-		image: '',
+		image: require('../../images/services/App.png'),
 		suits: [
-			'Professinals',
-			'Bands',
+			'Companies',
+			'Organizations',
 			'Businesses',
 			'Creatives',
-			'Retailers'
+			'Events',
 		],
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit purus dignissim porta dictum. Proin nec urna ut sem dictum condimentum eget sed tortor. Vivamus non enim at est luctus auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec porta sapien et arcu tincidunt varius. Suspendisse et scelerisque tellus. Nullam sed posuere nunc. Ut vel risus ligula. Sed a lacus id neque varius mattis. Cras vel lacus neque. Curabitur dictum neque scelerisque ex ultricies molestie. Integer quis ipsum sed massa rutrum euismod. Phasellus posuere massa ac risus posuere vestibulum. Nam quis ante mauris. Duis tellus est, aliquam in mauris nec, vehicula consectetur dui. Etiam bibendum eget quam quis porta. '
+		included: [
+			'Design',
+			'Development',
+			'App Store and Google Play Submssion',
+			'Updates',
+		],
+		description: `A Custom Enterprise App for your business. Apps functionality could range from Static Content, Dynamic Content, AR, and even where possible POS Systems.
+		\nAn App is a excellent way to optimize the efficiency of your day to day business. Or help customers connect to you in an engaging way.
+		\nAll apps are designed to look and feel amazing on both IOS and Android.
+		`
 	},
 ]
 
