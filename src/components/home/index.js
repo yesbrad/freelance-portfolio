d5a6354b01fe69e8f62283a4e25b405ae897360d
brad <b.bradfrancis@gmail.com>
2020-07-31 18:13:51 +1000

    Added Hamburger Navigation

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import Button from '../button';
 import { Link } from 'react-scroll';
+import { IoMdMenu, IoIosClose } from 'react-icons/io';
+import { useSpring, animated } from 'react-spring';
 
 const Home = () => {
+	const [isMenuOpen, SetIsMenuOpen] = useState(false);
+
+	const menuSpringProps = useSpring({
+		top: isMenuOpen ? '0' : '-50%',
+		bottom: isMenuOpen ? '50%' : '100%',
+	})
+
 	return (
 		<div className="home-container">
+			<animated.div style={menuSpringProps} className="home-hamburger-menu-container">
+				<button onClick={() => SetIsMenuOpen(false)}>
+					<IoIosClose style={{width: '100%', height: '100%'}}/>
+				</button>
+				<Link to="services" className="home-menu-button" smooth>SERVICES</Link>
+				<Link to="works"    className="home-menu-button" smooth>WORKS</Link>
+				<Link to="about"    className="home-menu-button" smooth>ABOUT</Link>
+				<Link to="contact" className="home-menu-button" smooth>CONTACT</Link>
+			</animated.div>
 			<div className="home-content-container">
 				<div class="home-content-gradient">
 					<div className="home-content-header">
@@ -19,13 +37,13 @@ const Home = () => {
 							<Link to="about" className="home-content-nav-button" smooth>ABOUT</Link>
 							<div className="home-content-nav-action"><Button isNav to="contact" title="Get a Quote"/></div>
 						</div>
+						<button className="home-content-hamburger-button" onClick={() => SetIsMenuOpen(true)}>
+							<IoMdMenu style={{width: '100%', height: '100%'}}/>
+						</button>
 					</nav>
 					<div className="home-content-action-buttons">
 						<div className="home-content-button">
 							<Button title="Get a Quote" isNav to="contact"/>
-						</div>
-						<div className="home-content-button" id="home-content-removed">
-							<Button isNav to="contact" title="Services" />
 						</div>
 					</div>
 					<div className="home-content-hook">
